cfab002139bf12e90540046f990209ef3aca5387
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-04-22 23:16:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import re
 
 
 # bot
-bot_token = os.environ.get("TOKEN", "6033306990:AAHlGOo2JtADZaCYT6I0d-yAbRLGYzm5VkU")
+bot_token = os.environ.get("TOKEN", "6262139986:AAEcWGlRAhXVP76iUiklSZLoHkvybSFkD2Q")
 api_hash = os.environ.get("HASH", "20ca7e6687c281e11782856c7efd0ff7") 
 api_id = os.environ.get("ID", "26305247")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
