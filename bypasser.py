5714d7386404e2e9f2851c7229d7d979b2ac7eae
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-04-24 11:43:33 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -464,30 +464,7 @@ def kolop_dl(url,kcrypt):
 ################################################################
 
 #################################################################
-# gdtot
-
-def gdtot(url: str, GdTot_Crypt: str) -> str:
-    client = requests.Session()
-    client.cookies.update({"crypt": GdTot_Crypt})
-    res = client.get(url)
-    base_url = re.match('^.+?[^\/:](?=[?\/]|$\n)', url).group(0)
-    res = client.get(f"{base_url}/dld?id={url.split('/')[-1]}")
-    url = re.findall(r'URL=(.*?)"', res.text)[0]
-    info = {}
-    info["error"] = False
-    params = parse_qs(urlparse(url).query)
-    if "gd" not in params or not params["gd"] or params["gd"][0] == "false":
-        info["error"] = True
-        if "msgx" in params:
-            info["message"] = params["msgx"][0]
-        else:
-            info["message"] = "Invalid link"
-    else:
-        decoded_id = base64.b64decode(str(params["gd"][0])).decode("utf-8")
-        drive_link = f"https://drive.google.com/open?id={decoded_id}"
-        info["gdrive_link"] = drive_link
-    if not info["error"]: return info["gdrive_link"]
-    else: return ddl.gdtot(url)
+# 
 
 
 ##################################################################
