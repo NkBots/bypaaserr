c377cc358af1e5f980a7e514debc84f0b3c412e9
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-04-24 10:47:52 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -754,25 +754,6 @@ def flashlink(url):
 #######################################################
 # short2url
 
-def short2url(url):
-    client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://techyuth.xyz/blog"
-    url = url[:-1] if url[-1] == '/' else url
-    code = url.split("/")[-1]
-    final_url = f"{DOMAIN}/{code}"
-    ref = "https://blog.coin2pay.xyz/"
-    h = {"referer": ref}
-    resp = client.get(final_url, headers=h)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    inputs = soup.find_all("input")
-    data = { input.get('name'): input.get('value') for input in inputs }
-    h = { "x-requested-with": "XMLHttpRequest" }
-    time.sleep(10)
-    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try:
-        return r.json()['url']
-    except: return "Something went wrong :("
-
 
 #######################################################
 # anonfiles
@@ -1653,10 +1634,7 @@ def shortners(url):
         print("entered flashlink:",url)
         return flashlink(url)
 
-    # short2url
-    elif "https://short2url.in/" in url:
-        print("entered short2url:",url)
-        return short2url(url)
+    
         
     # shorte
     elif "https://shorte.st/" in url:
